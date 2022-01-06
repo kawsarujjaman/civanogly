32d520d8c86eee9170edc45abb74e627239d02aa
Kawsarujjaman <72822486+kawsarujjaman@users.noreply.github.com>
2022-01-07 01:11:17 +0600

    Update property.php

--- app/Models/property.php
+++ app/Models/property.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Http;
+use App\Models\media;
 
 class property extends Model
 {
